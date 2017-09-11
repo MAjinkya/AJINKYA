a9c8ce735ea337d5fc2319d9f7ca15cfd04c5fec
Ajinkya Madhyeswar <ajinkya.q7@gmail.com>
2017-09-11 09:34:17 -0700

    files added

---
+++ JavaTutorial/Java_Learning/src/ClassDemo.java
@@ -0,0 +1,16 @@
+
+public class ClassDemo {
+	
+	String str2 = "Hi There !!!";
+
+	public static void main(String[] args) {
+		String str1 = "Hello Java";
+		
+		System.out.println(str1);
+		
+		for (int i=0; i < args.length; i++) {
+			System.out.println(args[i]);
+			
+		}
+	}
+}
---
+++ JavaTutorial/Java_Learning/src/Variables.java
@@ -0,0 +1,76 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		/*
+		 * Byte
+		 * Min: -128
+		 * Max: 127
+		 * Default: 0
+		 */
+		byte myByte = 100;
+		System.out.println("Byte Value: " + myByte);
+		
+		/*
+		 * Short
+		 * Min: -32,768
+		 * Max: 32,767
+		 * Default: 0
+		 */
+		short myShort = 10000;
+		System.out.println("Short Value: " + myShort);
+
+		
+		/*
+		 * int
+		 * Min: -2,147,483,648
+		 * Max: 2,147,483,647
+		 * Default: 0
+		 */
+		int myInt = 467;
+		System.out.println("Int Value is: " + myInt);
+		
+		/*
+		 * long
+		 * Min: -2^63
+		 * Max: 2^63 -1
+		 * Default: 0
+		 */
+	long myLong = 81;
+	System.out.println("Long value is:" + myLong);
+	
+
+		
+		/*
+		 * float
+		 * Default: 0.0f
+		 */
+	float myFloat = 0.07;
+	System.out.println("Float value is:" + myFloat);
+
+		 
+		 /*
+		  * double
+		  * Default: 0.0d
+		  */
+	double myDouble = 0.045;
+	System.out.println("Double value is:" + myDouble);
+		 
+		 /*
+		  * boolean
+		  * false and true
+		  * Default: false
+		  */
+		boolean myBoolean = true;
+		System.out.println("Boolean value is:" + myBoolean);
+		 
+		 /*
+		  * char
+		  * Min: 0
+		  * Max: 65,535
+		  */
+		char myChar = hello;
+		System.out.println("Char value is" + myChar);
+		
+	}
+}
